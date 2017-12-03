73c107d20a7e19064903799fd55beba44e74fc21
jfernandezpe <joseluisfp100@hotmail.com>
2017-12-03 19:30:01 +0100

    Feature: blacklist

--- src/endPoints/EndPoints.js
+++ src/endPoints/EndPoints.js
@@ -1,5 +1,5 @@
-// let base = "http://vps485915.ovh.net/api"
-let base = "http://localhost/ego/public"
+let base = "http://vps485915.ovh.net/api"
+// let base = "http://localhost/ego/public"
 export const EndPoints = {
   URL: {
     bestYouthPlayer: base+"/bestYouthPlayer",
